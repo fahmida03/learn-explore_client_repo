68f7d3fda3ce0e9dbf4ddd67071b594e9c44704b
Fahmida Muntasim <fahmidamuntasimt@gmail.com>
2022-10-25 00:43:08 +0600

    nav

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Blog from "../../Pages/Blog/Blog";
+import Courses from "../../Pages/Courses/Courses";
+import FAQ from "../../Pages/FAQ/FAQ";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
 
 export const routes = createBrowserRouter([
     {
@@ -10,6 +14,23 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+
             }
 
         ]
